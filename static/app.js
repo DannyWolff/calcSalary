c2a5b8c150518a05fcfefe28efbefc05b517c861
Danny Wolff <dannywolff.git@gmail.com>
2020-03-24 12:49:57 +0200

    remove all traces of loading spinner

--- static/app.js
+++ static/app.js
@@ -1,8 +1,4 @@
 document.getElementById('salary-form').addEventListener('submit', (e) => {
-    // document.getElementById('result').style.display = 'none';
-
-    // document.getElementById('loading').style.display = 'block';
-
      setTimeout(calcSal, 500);
     e.preventDefault();
 });
@@ -63,14 +59,13 @@ function calcSal() {
         wp.value = day_worked;
         wd.value=wkdys;
         
-        // document.getElementById('loading').style.display = 'none';
         document.getElementById('result').style.display = 'block';
     }
 }
 
 
 function showError(error) {
-    // document.getElementById('loading').style.display = 'none';
+
     document.getElementById('result').style.display = 'none';
     const errorDiv = document.createElement('div');
 
